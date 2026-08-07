8e33a6bc8b9107749ab8f11cad58db811f8d2b7a
kbmcanopy-devoc <kbmcanopy@gmail.com>
2026-08-07 10:14:26 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("NSTV Website Loaded");
+document.querySelectorAll(".btn-primary,.btn-download,.card,.gallery img")
+.forEach(item=>{
+
+item.addEventListener("mouseenter",()=>{
+
+item.style.transition="0.3s";
+
+});
+
+});
+
+console.log("NSTV Official Website");
